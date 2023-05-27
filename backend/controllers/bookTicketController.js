245200b65c1a5cf0c03db93b92f6d8154ed05c5d
RajatsGit <115529699+RajatsGit@users.noreply.github.com>
2023-05-27 20:03:37 +0530

    api
    Signed-off-by: RajatsGit <115529699+RajatsGit@users.noreply.github.com>

--- backend/controllers/bookTicketController.js
+++ backend/controllers/bookTicketController.js
@@ -2,6 +2,29 @@ const SeatModel = require('../models/seatBooking')
 
 const bookTicketController =async(req,res)=>{
     try {
+
+        // Check if there are any seats in the database
+  const existingSeats = await Seat.find();
+  if (existingSeats.length === 0) {
+    // If no seats exist, create 80 default seats
+    const defaultSeats = [];
+    for (let i = 1; i <= 80; i++) {
+      defaultSeats.push({ seatNumber: `Seat ${i}` });
+    }
+    Seat.insertMany(defaultSeats)
+      .then(() => {
+        console.log('Default seats added successfully');
+      })
+      .catch((error) => {
+        console.error('Error adding default seats:', error);
+      });
+  }
+
+
+
+
+
+
         // Number of seats that user requested
         const numberOfSeats = req.body.numberOfSeats;
 
@@ -11,7 +34,19 @@ const bookTicketController =async(req,res)=>{
         // Check if requested seats are avaliable or not
         if(availableSeats.length >= numberOfSeats){
             const bookedSeats = availableSeats.slice(0, numberOfSeats);
-            
+          
+        //   here simply booking seats and saving them in same function
+            bookedSeats.forEach((seat)=>{
+                seat.isBooked = true;
+                seat.save();
+            })
+
+            return(
+                res.status(200).send({
+                    success:true,
+                    message:'Seats Booked Succesfully'
+                })
+            )
 
         } else{
             return(
